a0adc713c74a84f817f3ecc19261c6fea38d902a
vichy130 <67213209+vichy130@users.noreply.github.com>
2024-07-11 19:45:08 -0600

    tareas

---
+++ Tarea/src/tarea/Main.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tarea;
+
+/**
+ *
+ * @author vichy
+ */
+public class Main {
+
+    public static void main(String args[]) {
+        Tarea newTarea1 = new Tarea();
+        newTarea1.add(3);
+        newTarea1.add(2);
+        newTarea1.add(4);
+        newTarea1.add(4);
+        newTarea1.add(1);
+        newTarea1.print();
+    }
+}
